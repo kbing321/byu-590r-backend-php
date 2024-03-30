36095279c661c08db0f62257b6b5bbdba3938a48
Kohlton Bingham <kohltonhbingham@gmail.com>
2024-03-30 12:11:06 -0600

    updates

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +17,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+    Route::post('logout', 'logout');
 });
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::controller(UserController::class)->group(function(){
+        Route::get('user', 'getUser');
+        Route::post('user/upload_avatar', 'uploadAvatar');
+        Route::delete('user/remove_avatar','removeAvatar');
+        Route::post('user/send_verification_email','sendVerificationEmail');
+        Route::post('user/change_email', 'changeEmail');
+    });
+
+    Route::resource('movies', MovieController::class);
+    Route::controller(MovieController::class)->group(function(){
+        Route::post('movies/{id}/update_movie_picture', 'updateMoviePicture');
+    });
+});
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
